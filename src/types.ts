fc0fa39846cf17570b8feb8aeab0fc7ad4f9781f
Mao <lcdsmao@gmail.com>
2020-10-03 15:54:09 +0900

    Basic types

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+export type SiteMetadata = {
+  title: string
+  description: string
+  siteUrl: string
+  social: {
+    twitter: string
+    github: string
+  }
+}
+
+export type Frontmatter = {
+  title: string
+  date: string
+  tags: string[]
+}
+
+export type MarkdownRemark = {
+  id: string
+  excerpt: string
+  html: string
+  frontmatter: Frontmatter
+  fields: {
+    slug: string
+  }
+}
