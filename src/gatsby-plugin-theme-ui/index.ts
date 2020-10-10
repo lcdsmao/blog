7a5052e24c95177d424ae7611d3bcc716ddd8537
Mao <lcdsmao@gmail.com>
2020-10-10 22:28:08 +0900

    Add Theme-ui prism plugin

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -1,14 +1,16 @@
+const colors = {
+  text: "hsl(10, 20%, 20%)",
+  background: "hsl(10, 10%, 98%)",
+  primary: "hsl(10, 80%, 50%)",
+  secondary: "hsl(10, 60%, 50%)",
+  highlight: "hsl(10, 40%, 90%)",
+  purple: "hsl(250, 60%, 30%)",
+  muted: "hsl(10, 20%, 94%)",
+  gray: "hsl(10, 20%, 50%)",
+}
+
 const theme = {
-  colors: {
-    text: "hsl(10, 20%, 20%)",
-    background: "hsl(10, 10%, 98%)",
-    primary: "hsl(10, 80%, 50%)",
-    secondary: "hsl(10, 60%, 50%)",
-    highlight: "hsl(10, 40%, 90%)",
-    purple: "hsl(250, 60%, 30%)",
-    muted: "hsl(10, 20%, 94%)",
-    gray: "hsl(10, 20%, 50%)",
-  },
+  colors: colors,
   fonts: {
     body:
       'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
