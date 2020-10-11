c3e41976cd3979dc544cda7a2e07d68475a0e37a
Mao <lcdsmao@gmail.com>
2020-10-11 21:24:28 +0900

    Avoid show dark toogle before valid mode value

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,6 @@ type Props = {
 }
 
 const Header: React.FC<Props> = ({ isHome, metadata }) => {
-  const [colorMode, setColorMode] = useColorMode()
   return (
     <Container
       sx={{
@@ -44,43 +43,52 @@ const Header: React.FC<Props> = ({ isHome, metadata }) => {
         <a href={`https://github.com/${metadata.social.github}`}>Github</a>
         <a href={`https://twitter.com/${metadata.social.twitter}`}>Twitter</a>
       </div>
-
-      <button
-        sx={{
-          background: "inherit",
-          border: "none",
-          p: 1,
-          outline: "inherit",
-          opacity: 0.7,
-          "&:hover": {
-            opacity: 1,
-          },
-        }}
-        title={`Toggle ${colorMode === "default" ? "Dark" : "Light"}`}
-        onClick={_ => {
-          setColorMode(colorMode === "default" ? "dark" : "default")
-        }}
-      >
-        {colorMode === "default" ? (
-          <SunIcon
-            sx={{
-              width: "1.5rem",
-              height: "1.5rem",
-              color: "text",
-            }}
-          />
-        ) : (
-          <MoonIcon
-            sx={{
-              width: "1.5rem",
-              height: "1.5rem",
-              color: "text",
-            }}
-          />
-        )}
-      </button>
+      <DarkToggle />
     </Container>
   )
 }
 
 export default Header
+
+const DarkToggle: React.FC = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  if (!colorMode) {
+    return null
+  }
+  return (
+    <button
+      sx={{
+        background: "inherit",
+        border: "none",
+        p: 1,
+        outline: "inherit",
+        opacity: 0.7,
+        "&:hover": {
+          opacity: 1,
+        },
+      }}
+      title={`Toggle ${colorMode === "default" ? "Dark" : "Light"}`}
+      onClick={_ => {
+        setColorMode(colorMode === "default" ? "dark" : "default")
+      }}
+    >
+      {colorMode === "default" ? (
+        <SunIcon
+          sx={{
+            width: "1.5rem",
+            height: "1.5rem",
+            color: "text",
+          }}
+        />
+      ) : (
+        <MoonIcon
+          sx={{
+            width: "1.5rem",
+            height: "1.5rem",
+            color: "text",
+          }}
+        />
+      )}
+    </button>
+  )
+}
