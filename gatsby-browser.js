78ee2655b2089e8a6929fdf0a65d83cf1fba022e
Mao <lcdsmao@gmail.com>
2020-10-17 17:36:12 +0900

    Add firebase analytics

---
+++ gatsby-browser.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app"
+import "firebase/analytics"
+
+export const onClientEntry = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyA0NSf6LngNsgu5Dkp0iWubzgV0fVPbW2M",
+    authDomain: "lcdsmao-blog.firebaseapp.com",
+    databaseURL: "https://lcdsmao-blog.firebaseio.com",
+    projectId: "lcdsmao-blog",
+    storageBucket: "lcdsmao-blog.appspot.com",
+    messagingSenderId: "574618045726",
+    appId: "1:574618045726:web:f60fdabe7a0b771465cd0f",
+    measurementId: "G-8NP75V6LP7",
+  }
+  firebase.initializeApp(firebaseConfig)
+  firebase.analytics()
+}
