54d10127ffd701724fda839541919a75a14a86bb
Mao <lcdsmao@gmail.com>
2020-10-18 10:19:44 +0900

    PWA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -15,3 +15,13 @@ export const onClientEntry = () => {
   firebase.initializeApp(firebaseConfig)
   firebase.analytics()
 }
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
